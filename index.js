3321b25d3ee7fc66c8a2488492589355786c170e
Ela <micaela.francisco.s@bulsu.edu.ph>
2024-03-22 01:10:07 +0800

    add joi-object id validation function

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")(app);
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
